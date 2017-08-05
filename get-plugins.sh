4d5a01963e048d001b555271b200c8ab8d6710ca
Joost van der Griendt <j.vandergriendt@flusso.nl>
2017-08-05 12:35:10 +0200

    Update get command

--- get-plugins.sh
+++ get-plugins.sh
@@ -7,4 +7,4 @@ USER="${1:-$USER_DEFAULT}"
 SERVER_DEFAULT="http://localhost:8080"
 SERVER="${2:-$SERVER_DEFAULT}"
 
-curl -u $USER -p -sSL "${SERVER}/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g'|sed 's/ /:/' > plugins-new.txt
+curl -k -u $USER -p -sSL "${SERVER}/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g'|sed 's/ /:/' > plugins-new.txt
